719fbc80d0f18d92acec836611cfee2d15eaab84
xa3687va <xander.van.der.weken@fh-erfurt.de>
2024-11-04 16:32:18 +0100

    Adding Delete and List in FileService

--- cmd/clean.go
+++ cmd/clean.go
@@ -1,19 +1,35 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/xandervanderweken/FolderCleaner/internal/services"
 )
 
 var cleanCmd = &cobra.Command{
 	Use:   "clean",
 	Short: "Clean specified files from a folder",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("clean called")
+		fileService := services.NewFileService()
+
+		var tempExtensions []string = []string{".go"}
+
+		if isDryRun {
+			fileService.ListFiles(path, tempExtensions)
+		} else {
+			fileService.DeleteFiles(path, tempExtensions)
+		}
 	},
 }
 
+var path string
+var isDryRun bool
+
 func init() {
 	rootCmd.AddCommand(cleanCmd)
+
+	cleanCmd.Flags().StringVarP(&path, "path", "p", "", "Path of folder")
+	cleanCmd.MarkFlagDirname("path")
+	cleanCmd.MarkFlagRequired("path")
+
+	cleanCmd.Flags().BoolVarP(&isDryRun, "dry", "d", false, "Should not delete - Running without deleting")
 }
